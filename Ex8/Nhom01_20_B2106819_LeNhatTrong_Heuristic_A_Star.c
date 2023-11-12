0ddfbe1fbfd492929520f8355fc5a2117e595f52
Vitris128 <lenhattrong128@gmail.com>
2023-11-12 15:11:44 +0700

    MOREEEEEEEEEEEEEE

--- Ex8/Nhom01_20_B2106819_LeNhatTrong_Heuristic_A_Star.c
+++ Ex8/Nhom01_20_B2106819_LeNhatTrong_Heuristic_A_Star.c
@@ -9,19 +9,16 @@
 
 const char *action[] = {"First State", "Move cell EMPTY to UP", "Move cell EMPTY to DOWN", "Move cell EMPTY to LEFT", "Move cell EMPTY to RIGHT"};
 
-typedef struct State
-{
+typedef struct State {
     int eightPuzzel[ROWS][COLS];
     int emptyRow;
     int emptyCol;
 } State;
 
-void printState(State state)
-{
+void printState(State state) {
     int row, col;
     printf("\n--------\n");
-    for (row = 0; row < ROWS; row++)
-    {
+    for (row = 0; row < ROWS; row++) {
         for (col = 0; col < COLS; col++)
             printf("|%d ", state.eightPuzzel[row][col]);
         printf("|\n");
@@ -29,8 +26,7 @@ void printState(State state)
     printf("--------\n");
 }
 
-int compareStates(State state1, State state2)
-{
+int compareStates(State state1, State state2) {
     if (state1.emptyRow != state2.emptyRow || state1.emptyCol != state2.emptyCol)
         return 0;
     int row, col;
@@ -41,17 +37,14 @@ int compareStates(State state1, State state2)
     return 1;
 }
 
-int goalCheck(State state, State goal)
-{
+int goalCheck(State state, State goal) {
     return compareStates(state, goal);
 }
 
-int upOperator(State state, State *result)
-{
+int upOperator(State state, State *result) {
     *result = state;
     int empRowCurrent = state.emptyRow, empColCurrent = state.emptyCol;
-    if (empRowCurrent > 0)
-    {
+    if (empRowCurrent > 0) {
         result->emptyRow = empRowCurrent - 1;
         result->emptyCol = empColCurrent;
         result->eightPuzzel[empRowCurrent][empColCurrent] = state.eightPuzzel[empRowCurrent - 1][empColCurrent];
@@ -60,12 +53,10 @@ int upOperator(State state, State *result)
     }
     return 0;
 }
-int downOperator(State state, State *result)
-{
+int downOperator(State state, State *result) {
     *result = state;
     int empRowCurrent = state.emptyRow, empColCurrent = state.emptyCol;
-    if (empRowCurrent < ROWS - 1)
-    {
+    if (empRowCurrent < ROWS - 1) {
         result->emptyRow = empRowCurrent + 1;
         result->emptyCol = empColCurrent;
         result->eightPuzzel[empRowCurrent][empColCurrent] = state.eightPuzzel[empRowCurrent + 1][empColCurrent];
@@ -74,12 +65,10 @@ int downOperator(State state, State *result)
     }
     return 0;
 }
-int leftOperator(State state, State *result)
-{
+int leftOperator(State state, State *result) {
     *result = state;
     int empRowCurrent = state.emptyRow, empColCurrent = state.emptyCol;
-    if (empColCurrent > 0)
-    {
+    if (empColCurrent > 0) {
         result->emptyRow = empRowCurrent;
         result->emptyCol = empColCurrent - 1;
         result->eightPuzzel[empRowCurrent][empColCurrent] = state.eightPuzzel[empRowCurrent][empColCurrent - 1];
@@ -88,12 +77,10 @@ int leftOperator(State state, State *result)
     }
     return 0;
 }
-int rightOperator(State state, State *result)
-{
+int rightOperator(State state, State *result) {
     *result = state;
     int empRowCurrent = state.emptyRow, empColCurrent = state.emptyCol;
-    if (empColCurrent < COLS - 1)
-    {
+    if (empColCurrent < COLS - 1) {
         result->emptyRow = empRowCurrent;
         result->emptyCol = empColCurrent + 1;
         result->eightPuzzel[empRowCurrent][empColCurrent] = state.eightPuzzel[empRowCurrent][empColCurrent + 1];
@@ -103,25 +90,22 @@ int rightOperator(State state, State *result)
     return 0;
 }
 
-int callOperators(State state, State *result, int opt)
-{
-    switch (opt)
-    {
-    case 1:
-        return upOperator(state, result);
-    case 2:
-        return downOperator(state, result);
-    case 3:
-        return leftOperator(state, result);
-    case 4:
-        return rightOperator(state, result);
-    default:
-        printf("Cannot call operators");
-        return 0;
+int callOperators(State state, State *result, int opt) {
+    switch (opt) {
+        case 1:
+            return upOperator(state, result);
+        case 2:
+            return downOperator(state, result);
+        case 3:
+            return leftOperator(state, result);
+        case 4:
+            return rightOperator(state, result);
+        default:
+            printf("Cannot call operators");
+            return 0;
     }
 }
-int heuristicOne(State state, State goal)
-{
+int heuristicOne(State state, State goal) {
     int row, col, count = 0;
     for (row = 0; row < ROWS; row++)
         for (col = 0; col < COLS; col++)
@@ -129,8 +113,7 @@ int heuristicOne(State state, State goal)
                 count++;
     return count;
 }
-typedef struct Node
-{
+typedef struct Node {
     State state;
     struct Node *parent;
     int no_function;
@@ -139,50 +122,40 @@ typedef struct Node
     int h;
 } Node;
 
-typedef struct List
-{
+typedef struct List {
     Node *Elements[MAXLENGTH];
     int size;
 } List;
 
-void makeNULLList(List *list)
-{
+void makeNULLList(List *list) {
     list->size = 0;
 }
-int emptyList(List list)
-{
+int emptyList(List list) {
     return list.size == 0;
 }
-int fullList(List list)
-{
+int fullList(List list) {
     return list.size == MAXLENGTH;
 }
-Node *elementAt(int p, List list)
-{
+Node *elementAt(int p, List list) {
     return list.Elements[p - 1];
 }
-void pushList(Node *x, int position, List *list)
-{
-    if (!fullList(*list))
-    {
+void pushList(Node *x, int position, List *list) {
+    if (!fullList(*list)) {
         int q;
         for (q = list->size; q >= position; q--)
             list->Elements[q] = list->Elements[q - 1];
         list->Elements[position - 1] = x;
         list->size++;
-    }
-    else
+    } else
         printf("List is full\n");
 }
 
-void deleteList(int position, List *list)
-{
+void deleteList(int position, List *list) {
     if (emptyList(*list))
         printf("List is empty\n");
     else if (position < 1 || position > list->size)
         printf("Position is not possible to delete\n");
-    else
-    {
+    else {
         int i;
         for (i = position - 1; i < list->size; i++)
             list->Elements[i] = list->Elements[i + 1];
@@ -190,33 +163,28 @@ void deleteList(int position, List *list)
     }
 }
 
-Node *findState(State state, List list, int *position)
-{
+Node *findState(State state, List list, int *position) {
     int i;
     for (i = 1; i <= list.size; i++)
-        if (compareStates(elementAt(i, list)->state, state))
-        {
+        if (compareStates(elementAt(i, list)->state, state)) {
             *position = i;
             return elementAt(i, list);
         }
     return NULL;
 }
 
-void sortList(List *list)
-{
+void sortList(List *list) {
     int i, j;
     for (i = 0; i < list->size - 1; i++)
         for (j = i + 1; j < list->size; j++)
-            if (list->Elements[i]->f > list->Elements[j]->f)
-            {
+            if (list->Elements[i]->f > list->Elements[j]->f) {
                 Node *node = list->Elements[i];
                 list->Elements[i] = list->Elements[j];
                 list->Elements[j] = node;
             }
 }
 
-Node *A_Star(State state, State goal)
-{
+Node *A_Star(State state, State goal) {
     List Open_A_Star, Close_A_Star;
     makeNULLList(&Open_A_Star);
     makeNULLList(&Close_A_Star);
@@ -228,20 +196,17 @@ Node *A_Star(State state, State goal)
     root->h = heuristicOne(root->state, goal);
     root->f = root->g + root->h;
     pushList(root, Open_A_Star.size + 1, &Open_A_Star);
-    while (!emptyList(Open_A_Star))
-    {
+    while (!emptyList(Open_A_Star)) {
         Node *node = elementAt(1, Open_A_Star);
         deleteList(1, &Open_A_Star);
         pushList(node, Close_A_Star.size + 1, &Close_A_Star);
         if (goalCheck(node->state, goal))
             return node;
         int opt;
-        for (opt = 1; opt <= MAX_OPERATOR; opt++)
-        {
+        for (opt = 1; opt <= MAX_OPERATOR; opt++) {
             State newstate;
-            if (callOperators(node->state, &newstate, opt))
-            {
-                Node* newNode = (Node*)malloc(sizeof(Node));
+            if (callOperators(node->state, &newstate, opt)) {
+                Node *newNode = (Node *)malloc(sizeof(Node));
                 newNode->state = newstate;
                 newNode->parent = node;
                 newNode->no_function = opt;
@@ -249,19 +214,14 @@ Node *A_Star(State state, State goal)
                 newNode->h = heuristicOne(newstate, goal);
                 newNode->f = newNode->g + newNode->h;
                 int posOpen, posClose;
-                Node* nodeFoundOpen = findState(newstate, Open_A_Star, &posOpen);
-                Node* nodeFoundClose = findState(newstate, Close_A_Star, &posClose);
-                if (nodeFoundOpen == NULL && nodeFoundClose == NULL)
-                {
+                Node *nodeFoundOpen = findState(newstate, Open_A_Star, &posOpen);
+                Node *nodeFoundClose = findState(newstate, Close_A_Star, &posClose);
+                if (nodeFoundOpen == NULL && nodeFoundClose == NULL) {
                     pushList(newNode, Open_A_Star.size + 1, &Open_A_Star);
-                }
-                else if (nodeFoundOpen != NULL && nodeFoundOpen->g > newNode->g)
-                {
+                } else if (nodeFoundOpen != NULL && nodeFoundOpen->g > newNode->g) {
                     deleteList(posOpen, &Open_A_Star);
                     pushList(newNode, posOpen, &Open_A_Star);
-                }
-                else if (nodeFoundClose != NULL && nodeFoundClose->g > newNode->g)
-                {
+                } else if (nodeFoundClose != NULL && nodeFoundClose->g > newNode->g) {
                     deleteList(posClose, &Close_A_Star);
                     pushList(newNode, Open_A_Star.size + 1, &Open_A_Star);
                 }
@@ -272,27 +232,23 @@ Node *A_Star(State state, State goal)
     return NULL;
 }
 
-void printWaysToGetGoal(Node *node)
-{
+void printWaysToGetGoal(Node *node) {
     List listPrint;
     makeNULLList(&listPrint);
-    while (node->parent != NULL)
-    {
+    while (node->parent != NULL) {
         pushList(node, listPrint.size + 1, &listPrint);
         node = node->parent;
     }
     pushList(node, listPrint.size + 1, &listPrint);
 
     int no_action = 0, i;
-    for (i = listPrint.size; i > 0; i--)
-    {
+    for (i = listPrint.size; i > 0; i--) {
         printf("\nAction %d: %s", no_action, action[elementAt(i, listPrint)->no_function]);
         printState(elementAt(i, listPrint)->state);
         no_action++;
     }
 }
-int main()
-{
+int main() {
     State state, goal, result;
     state.emptyCol = 0;
     state.emptyRow = 1;
@@ -320,11 +276,10 @@ int main()
 
     Node *p = A_Star(state, goal);
 
-    if(p==NULL)
+    if (p == NULL)
         printf("Can't find the answer!!\n");
     else
-    printWaysToGetGoal(p);
-
+        printWaysToGetGoal(p);
 
     return 0;
 }
